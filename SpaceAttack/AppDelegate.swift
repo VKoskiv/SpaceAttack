553686e7374ae661b250b578a33ddde65f0ffe88
Valtteri Koskivuori <vkoskiv@gmail.com>
2017-06-05 18:28:43 +0300

    Initial Commit

---
+++ SpaceAttack/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  SpaceAttack
+//
+//  Created by Valtteri Koskivuori on 05/06/2017.
+//  Copyright © 2017 Valtteri Koskivuori. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
